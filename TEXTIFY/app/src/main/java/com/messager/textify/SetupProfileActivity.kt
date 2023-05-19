472edcce51dfa2dd9de1b0ebc0a03e437aaf6542
Theo Stana <68549484+thanax28@users.noreply.github.com>
2023-05-19 15:44:32 +0300

    SetupProfileActivity

--- TEXTIFY/app/src/main/java/com/messager/textify/SetupProfileActivity.kt
+++ TEXTIFY/app/src/main/java/com/messager/textify/SetupProfileActivity.kt
@@ -4,11 +4,45 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SetupProfileActivity : AppCompatActivity() {
+
+    // data base - posibil sa nu functioneze (FIREBASE nu e facut)
+    // min 41-ish
+    var binding:SetupProfileActivityBinding? = null
+    var auth:FirebaseAuth? = null
+    var database:FirebaseDatabase? = null
+    var storage:FirebaseStorage? = null
+    var selectedImage: Uri? = null
+    var dialog:ProgressDialog? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setup_profile)
+        binding = ActivitySetupProfileBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        dialog!!.setMessage("Updating Profile...")
+        dialog!!.setCancelable(false)
+        database = FirebaseDatabase.getInstance()
+        storage = FirebaseStorage.getInstance()
+        auth = FirebaseAuth.getInstance()
+        supportActionBar?.hide()
+        binding!!.imageView.setOnClickListener{
+            var intent = Intent()
+            intent.action = Intent.ACTION_GET_CONTENT
+            intent.type = "image/*"
+            startActivityForResult(intent,45)
+        }
+        binding.continueBtn02.setOnClickListener{
+            val name :String = binding.nameBox.text.toString()
+            if (name.isEmpty()){
+                binding!!.nameBox.setError("Please type a name")
+            }
+            dialog!!.show()
+            if (selectedImage != null){
+                val reference = storage.reference.child("Profile")
+                        .child(auth.uid!!)
+
+            }
+
+        }
+
     }
-
-
-    
 }
