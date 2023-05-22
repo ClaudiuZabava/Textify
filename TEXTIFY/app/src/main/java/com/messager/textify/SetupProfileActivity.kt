b882e595e2e135684806d406e6c4960e70f73d6d
ClaudiuZabava <zabavaclaudiu4@gmail.com>
2023-05-23 02:03:28 +0300

    Fixes

--- TEXTIFY/app/src/main/java/com/messager/textify/SetupProfileActivity.kt
+++ TEXTIFY/app/src/main/java/com/messager/textify/SetupProfileActivity.kt
@@ -1,18 +1,25 @@
 package com.messager.textify
 
+import android.app.ProgressDialog
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.messager.textify.databinding.ActivitySetupProfileBinding
 
 class SetupProfileActivity : AppCompatActivity() {
 
     // data base - posibil sa nu functioneze (FIREBASE nu e facut)
     // min 41-ish
-    var binding:SetupProfileActivityBinding? = null
-    var auth:FirebaseAuth? = null
-    var database:FirebaseDatabase? = null
-    var storage:FirebaseStorage? = null
+    var binding:ActivitySetupProfileBinding? = null
+    var auth: FirebaseAuth? = null
+    var database: FirebaseDatabase? = null
+    var storage: FirebaseStorage? = null
     var selectedImage: Uri? = null
-    var dialog:ProgressDialog? = null
+    var dialog: ProgressDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,15 +37,15 @@ class SetupProfileActivity : AppCompatActivity() {
             intent.type = "image/*"
             startActivityForResult(intent,45)
         }
-        binding.continueBtn02.setOnClickListener{
-            val name :String = binding.nameBox.text.toString()
+        binding!!.continueButton2.setOnClickListener{
+            val name :String = binding!!.nameBox.text.toString()
             if (name.isEmpty()){
-                binding!!.nameBox.setError("Please type a name")
+                binding!!.nameBox.error = "Please type a name"
             }
             dialog!!.show()
             if (selectedImage != null){
-                val reference = storage.reference.child("Profile")
-                        .child(auth.uid!!)
+                val reference = storage!!.reference.child("Profile")
+                        .child(auth!!.uid!!)
 
             }
 
